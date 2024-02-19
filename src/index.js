42d138af5e59063aca1e8626220b10c384439c82
Ethan Che <ethanche1@gmail.com>
2024-02-19 13:17:10 -0500

    How to run on localhost

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 const Colyseus = require("colyseus.js");
 
-var client = new Colyseus.Client('ws://scranton-sleuths-server-99a94c66225e.herokuapp.com/');
+/*  If you want to run the client on localhost, comment out the line below for the webserver and uncomment the one for localhost. Vice-versa for running on webserver.
+    ONLY ONE OF THE LINES BELOW SHOULD BE ACTIVE!!!!
+*/
+
+var client = new Colyseus.Client('ws://scranton-sleuths-server-99a94c66225e.herokuapp.com/'); //Use this for connecting to webserver
+//var client = new Colyseus.Client('ws://localhost:3000'); //Use this for connecting to localhost
+
+// --------------------------------------------------------------------------------------------------------------------------------------------
 
 client.joinOrCreate("my_room").then(room => {
     console.log(room.sessionId, "joined", room.name);
