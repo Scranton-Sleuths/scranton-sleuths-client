96a9adc9238f375fd5de8ed35ae5ace1cec37a99
Karen Cavicchio <karen.cavicchio@gmail.com>
2024-02-26 22:39:26 -0800

    Added multi client test to test 6 clients

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 const Colyseus = require("colyseus.js");
+var MultiClientTest = require("./multi-client-test");
 
 /*  If you want to run the client on localhost, comment out the line below for the webserver and uncomment the one for localhost. Vice-versa for running on webserver.
     ONLY ONE OF THE LINES BELOW SHOULD BE ACTIVE!!!!
 */
 
-var client = new Colyseus.Client('ws://scranton-sleuths-server-99a94c66225e.herokuapp.com/'); //Use this for connecting to webserver
-//var client = new Colyseus.Client('ws://localhost:3000'); //Use this for connecting to localhost
+//var client = new Colyseus.Client('ws://scranton-sleuths-server-99a94c66225e.herokuapp.com/'); //Use this for connecting to webserver
+var client = new Colyseus.Client('ws://localhost:3000'); //Use this for connecting to localhost
 
 // --------------------------------------------------------------------------------------------------------------------------------------------
 
-client.joinOrCreate("my_room").then(room => {
-    console.log(room.sessionId, "joined", room.name);
-}).catch(e => {
-    console.log("JOIN ERROR", e);
-});
+console.log("Hello, Detective.");
+
+MultiClientTest.test_n_clients(6);
---
+++ src/multi-client-test.js
@@ -0,0 +1,20 @@
+const Colyseus = require("colyseus.js");
+
+// Note: uncomment the first line if you wish to test the actual server; the second to test your local server
+//const SERVER_URL = 'ws://scranton-sleuths-server-99a94c66225e.herokuapp.com/';
+const SERVER_URL = 'ws://localhost:3000';
+
+const MultiClientTest = {
+    test_n_clients: function(n) {
+        for (var i = 0; i < n; i++) {
+            var client_i = new Colyseus.Client(SERVER_URL);
+            client_i.joinOrCreate("my_room").then(room => {
+                console.log(room.sessionId, "joined", room.name);
+            }).catch(e => {
+                console.log("JOIN ERROR", e);
+            });
+        }
+    }
+}
+
+module.exports = MultiClientTest;
